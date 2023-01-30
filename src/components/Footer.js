637403f4c2a8bbd72df4c7b77a134b528c4eb8b1
Minh Truong <mptruong49@gmail.com>
2023-01-30 15:45:59 -0700

    Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { VscGithubAlt } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaTwitter } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer>
+      <h2>Made with ♥ by Minh Truong</h2>
+      <div>
+        <ul>
+          <a href="https://github.com/RyanTrian">
+            <li className="logo">
+              <VscGithubAlt />
+            </li>
+          </a>
+          <a href="https://www.linkedin.com/in/minhtruong49/">
+            <li className="logo">
+              <AiOutlineLinkedin />
+            </li>
+          </a>
+          <a href="https://twitter.com/TruogMinh">
+            <li className="logo">
+              <FaTwitter />
+            </li>
+          </a>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
