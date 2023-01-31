bcba48e11127e63fac596611c42b9d93f81e1122
Minh Truong <mptruong49@gmail.com>
2023-01-31 13:37:16 -0700

    Portfolio Component, and helper function

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    link: "https://kipschetnan.github.io/Marvelite-Hub/",
+    src: "https://user-images.githubusercontent.com/82792300/196270573-0d7dacbd-9142-4cef-a72b-5da9d2d994bd.png",
+    name: "Marvelite Hub",
+    description: `Marvelite Hub allows the user to search through a server-side database to find comics any given Marvel character appears in. The user is also able to 
+    search all films the character appears in within the Marvel Cinematic Universe through a second server-side API. The user's search history is saved to local storage 
+    for convenience, in case the user might need to refer back to a previous search.`
+  },
+  {
+    link: "https://gamebox.herokuapp.com/",
+    src: "https://user-images.githubusercontent.com/82792300/207756222-05a3f201-dc08-40c8-9ca8-f494fe3bca68.png",
+    name: "Gameboxd",
+    description: `An web app which user can create their own account (receive a welcome email send through Nodemailer), search for games from STEAM through a STEAM API. 
+    Then, inside the game details page, user can view the game's thumbnail, description, other users' reviews and rating fo the game. User can also give their own 
+    rating and review on the same page and if they like the game, it can be bookmark to their profile.`
+  },
+  {
+    link: "https://ryantrian.github.io/Weather-DashBoard-WK6/",
+    src: "https://user-images.githubusercontent.com/82792300/194737744-b0ece148-06c2-4bb3-a141-ee0b70451774.png",
+    name: "Weather Dashboard",
+    description: `With server-side APIs, I made a Weather Dashboard. Utilize OpenWeather APIs, weather data of target city can be rendered on the page. Data includes: 
+    current Weather Data, 5 Day / 3 Hour Forecast, and current weather icons.`
+  },
+  {
+    link: "https://github.com/RyanTrian/Note-Taker-WK11.git",
+    src: "https://user-images.githubusercontent.com/82792300/202019017-53c3d443-1a61-45fc-b021-390b10bae966.png",
+    name: "Note Taker",
+    description: `A note taking web app and deploy on Heroku. When user visits the app, they will be presented with a button that direct them to the notes page. User 
+    then can make a new note, save it. The note will be logged, user can review it and delete it.`
+  },
+  {
+    link: "https://ryantrian.github.io/Code-Quiz_WEBAPIs-WK4/",
+    src: "https://user-images.githubusercontent.com/82792300/193190591-655cf0f8-044b-4648-85e7-2e448f3b0e62.png",
+    name: "Harry Potter: Book 1 Quiz",
+    description: `Utilize Web APIs, user can interact with the buttons to start the quiz, display questions, choose answers, correct or wrong answer indications, save 
+    highscores and sort in the order of high to low score.`
+  },
+  // {
+  //   link: "",
+  //   src: "",
+  //   name: "",
+  //   description: ``
+  // },
+  // {
+  //   link: "",
+  //   src: "",
+  //   name: "",
+  //   description: ``
+  // },
+]
